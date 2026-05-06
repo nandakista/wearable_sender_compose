35135888949cfed41ad0a13a0a21facf3057ec83
nanda.permana <nanda.permana@japfa.com>
2026-05-06 11:08:14 +0700

    Add button Open App in notification

--- app/src/main/java/stp/app/explorewearable/presentation/helpers/WearableMessageListener.kt
+++ app/src/main/java/stp/app/explorewearable/presentation/helpers/WearableMessageListener.kt
@@ -2,10 +2,13 @@ package stp.app.explorewearable.presentation.helpers
 
 import android.app.NotificationChannel
 import android.app.NotificationManager
+import android.app.PendingIntent
+import android.content.Intent
 import android.util.Log
 import androidx.core.app.NotificationCompat
 import com.google.android.gms.wearable.MessageEvent
 import com.google.android.gms.wearable.WearableListenerService
+import stp.app.explorewearable.presentation.MainActivity
 
 class WearableMessageListener : WearableListenerService() {
     override fun onMessageReceived(messageEvent: MessageEvent) {
@@ -23,6 +26,17 @@ class WearableMessageListener : WearableListenerService() {
 
         val manager =
             getSystemService(NOTIFICATION_SERVICE) as NotificationManager
+
+        val intent = Intent(this, MainActivity::class.java).apply {
+            putExtra("message", message)
+        }
+        val pendingIntent = PendingIntent.getActivity(
+            this,
+            0,
+            intent,
+            PendingIntent.FLAG_UPDATE_CURRENT or PendingIntent.FLAG_IMMUTABLE
+        )
+
         val channel = NotificationChannel(
             channelId,
             "Default Channel",
@@ -36,6 +50,7 @@ class WearableMessageListener : WearableListenerService() {
             .setSmallIcon(android.R.drawable.ic_dialog_info)
             .setPriority(NotificationCompat.PRIORITY_MAX)
             .setDefaults(NotificationCompat.DEFAULT_ALL)
+            .setContentIntent(pendingIntent)
             .setAutoCancel(true)
             .build()
 
